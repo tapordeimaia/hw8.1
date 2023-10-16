7dacd761153870c64f961a950b3a20737ce73bd0
tapordeimaia <146013799+tapordeimaia@users.noreply.github.com>
2023-10-16 17:26:08 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Student someStudent = new Student("high school", 2023, 3000);
+        someStudent.setAddress("31 august");
+        someStudent.setName("Maia");
+        System.out.println(someStudent);
+    }
+}
---
+++ Person.java
@@ -0,0 +1,36 @@
+public class Person {
+    private String name;
+    private String address;
+
+    public Person(){
+
+    }
+    public Person(String name, String address){
+        this.name=name;
+        this.address=address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
